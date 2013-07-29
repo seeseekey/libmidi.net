f986f066f1af9d19c94fb21a41be738ac2d5780c
seeseekey <seeseekey@gmail.com>
2013-07-29 17:49:37 +0200

    Implement ToString method and == operator for Note class.

--- libmidi.net/Note.cs
+++ libmidi.net/Note.cs
@@ -15,6 +15,51 @@ namespace libmidi.net
 			Type = type;
 			Velocity = velocity;
 		}
+
+		public override string ToString()
+		{
+			string tones = "";
+			foreach(MidiNote note in Tones)
+			{
+				tones += note.ToString() + ", ";
+			}
+			tones.Trim().TrimEnd(',');
+
+			return string.Format("[Note: Tones={0}, Type={1}, Velocity={2}]", tones, Type, Velocity);
+		}
+
+		public static bool operator == (Note a, Note b)
+		{
+			if(a.Type != b.Type)
+				return false;
+			if(a.Velocity != b.Velocity)
+				return false;
+			if(a.Tones.Length != b.Tones.Length)
+				return false;
+
+			foreach(MidiNote noteB in b.Tones)
+			{
+				bool found = false;
+				foreach(MidiNote noteA in a.Tones)
+				{
+					if(noteA == noteB)
+					{
+						found = true;
+						break;
+					}
+
+					if(found == false)
+						return false;
+				}
+			}
+
+			return true;
+		}
+
+		public static bool operator != (Note a, Note b)
+		{
+			return !(a == b);
+		}
 	}
 }
 
